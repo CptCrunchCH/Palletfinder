6e4d6611704c27437b2b4220a619bb91bcd6af83
nvidia <flo.heimann@hotmail.com>
2022-07-01 14:07:36 +0200

    Cleaning Fix

--- Make_Prediction.sh
+++ Make_Prediction.sh
@@ -2,10 +2,10 @@
 
 # Snap Image:
 #   Make an Executable Python File:
-chmod 777 /home/nvidia/Palletfinder/05_Make_Prediction/Snap_Images.py
-chmod +x /home/nvidia/Palletfinder/05_Make_Prediction/Snap_Images.py
+chmod 777 /home/nvidia/Palletfinder/03_Make_Prediction/Snap_Images.py
+chmod +x /home/nvidia/Palletfinder/03_Make_Prediction/Snap_Images.py
 #   Snap image and save in /05_Make_Prediction/Images
-python3 /home/nvidia/Palletfinder/05_Make_Prediction/Snap_Images.py
+python3 /home/nvidia/Palletfinder/03_Make_Prediction/Snap_Images.py
 
 
 # Use docker to read model to make Prediction
@@ -15,6 +15,6 @@ sudo docker container rm Palletfinder
 #   Run nvidia docker
 sudo docker run --runtime nvidia -itd --name=Palletfinder --volume ~/Palletfinder:/home/Palletfinder nvcr.io/nvidia/l4t-ml:r32.6.1-py3
 #   Execute Prediction python file in docker
-sudo docker exec -it Palletfinder /bin/bash -c "chmod 777 /home/Palletfinder/05_Make_Prediction/Makeprediction.py"
-sudo docker exec -it Palletfinder /bin/bash -c "chmod +x /home/Palletfinder/05_Make_Prediction/Makeprediction.py"
-sudo docker exec -it Palletfinder /bin/bash -c "/usr/bin/python3.6 /home/Palletfinder/05_Make_Prediction/Makeprediction.py"
+sudo docker exec -it Palletfinder /bin/bash -c "chmod 777 /home/Palletfinder/03_Make_Prediction/Makeprediction.py"
+sudo docker exec -it Palletfinder /bin/bash -c "chmod +x /home/Palletfinder/03_Make_Prediction/Makeprediction.py"
+sudo docker exec -it Palletfinder /bin/bash -c "/usr/bin/python3.6 /home/Palletfinder/03_Make_Prediction/Makeprediction.py"
--- Snap_Images.sh
+++ Snap_Images.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-chmod 777 /home/nvidia/Palletfinder/05_Make_Prediction/Snap_Images.py
+chmod 777 /home/nvidia/Palletfinder/03_Make_Prediction/Snap_Images.py
 
-chmod +x /home/nvidia/Palletfinder/05_Make_Prediction/Snap_Images.py
+chmod +x /home/nvidia/Palletfinder/03_Make_Prediction/Snap_Images.py
 
-python3 /home/nvidia/Palletfinder/05_Make_Prediction/Snap_Images.py
+python3 /home/nvidia/Palletfinder/03_Make_Prediction/Snap_Images.py
