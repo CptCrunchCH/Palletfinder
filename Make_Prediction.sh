7d4988105d272e94d9a31450d0c70ff2c87c1732
nvidia <flo.heimann@hotmail.com>
2022-07-01 13:37:25 +0200

    Fix Docker Start

--- Make_Prediction.sh
+++ Make_Prediction.sh
@@ -9,7 +9,7 @@ python3 /home/nvidia/Palletfinder/05_Make_Prediction/Snap_Images.py
 sudo docker kill $(docker ps -q)
 sudo docker container rm Palletfinder
 
-sudo docker run --runtime nvidia -itd --name=Palletfinder --volume ~/Palletfinder:/home/Palletfinder nvcr.io/nvidia/l4t-ml:r32.6.1-py
+sudo docker run --runtime nvidia -itd --name=Palletfinder --volume ~/Palletfinder:/home/Palletfinder nvcr.io/nvidia/l4t-ml:r32.6.1-py3
 
 sudo docker exec -it Palletfinder /bin/bash -c "chmod 777 /home/Palletfinder/05_Make_Prediction/Makeprediction.py"
 
